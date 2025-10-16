05a763bc4ba833a1ce313a1167c3357c50e678be
Adithya Shenal <washenal55@gmail.com>
2025-10-17 00:13:32 +0530

    post_service files/dir created

--- post_service/index.js
+++ post_service/index.js
@@ -1,2 +1,29 @@
 import express from "express";
 const app = express();
+import mysql from "mysql2";
+
+app.use(express.json());
+
+// Create a Post
+app.post("/api/posts", (req, res) => {});
+
+const pool = mysql
+  .createPool({
+    host: "localhost",
+    user: "root",
+    password: "adithya123",
+    database: "lumina_postservicedb",
+  })
+  .promise();
+
+async function getPosts() {
+  const [rows] = await pool.query("SELECT * FROM posts");
+  console.log(rows);
+}
+
+getPosts();
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Listening to ${port}`);
+});
